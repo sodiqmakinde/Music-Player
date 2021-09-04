8b9aa4813d4390d3f8d723c2dcd8d30142c123aa
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-09-04 17:28:44 +0100

    Hambuger menu

---
+++ script.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".form");
+    const navLinks = document.querySelectorAll(".form li");
+
+    //toggle nav
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        //Animation Links
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = "";
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+    });
+ 
+}
+
+navSlide()
