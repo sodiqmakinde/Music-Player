0e73786d8404a94f704630b0eaf4f838af0064d6
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-09-05 21:56:14 +0100

    We have added the music

---
+++ music.js
@@ -0,0 +1,100 @@
+const musicContainer = document.querySelector(".music-container")
+const playBtn = document.querySelector("#play")
+const prevBtn = document.querySelector("#prev")
+const nextBtn = document.querySelector("#next")
+const audio = document.querySelector("#audio")
+const progress = document.querySelector(".progress")
+const progressContainer = document.querySelector(".progress-container")
+const title = document.querySelector("#title")
+const cover = document.querySelector("#cover")
+
+
+//song title
+const songs = ["Try Me","Kizz_Daniel_Lie"]
+
+//Keep track of songs
+let songIndex = 1
+
+//Initially Load songs info DOM
+loadSong(songs[songIndex])
+
+//Update Song Details
+function loadSong(song) {
+    title.innerText = song
+    audio.src = `musics/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+function playSong() {
+    musicContainer.classList.add("play")
+    playBtn.querySelector("i.fa").classList.remove("fa-play")
+    playBtn.querySelector("i.fa").classList.add("fa-pause")
+
+    audio.play()
+}
+function pauseSong() {
+    musicContainer.classList.remove("play")
+    playBtn.querySelector("i.fa").classList.add("fa-play")
+    playBtn.querySelector("i.fa").classList.remove("fa-pause")
+
+    audio.pause()
+}
+
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e) {
+    const { duration,currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+// Event Listeners
+playBtn.addEventListener("click", () => {
+    const isPlaying = musicContainer.classList.contains("play")
+
+    if(isPlaying){
+        pauseSong()
+    } else {
+        playSong()
+    }
+});
+
+//Change Song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress)
+
+progressContainer.addEventListener("click",setProgress)
+
+audio.addEventListener("ended",nextSong)
